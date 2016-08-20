53ec109b6e675c8cea7fdf74b6d2688dfa22dbe0
Mike <tidemill@hotmail.com>
2016-08-20 11:20:24 -0500

    more hello world

--- hello world/Program.cs
+++ hello world/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace ConsoleApplication
 {
@@ -6,7 +7,39 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
+            Debug.WriteLine("hellodebug");
             Console.WriteLine("Hello World!");
+            var handler = new CharacterHandler();
+            handler.Handle();
+            Console.WriteLine("finish!!!!!");
+        }
+    } //this is class end
+
+    public class CharacterHandler
+    {
+        public void Handle()
+        {
+            ConsoleKeyInfo? x = null;
+            int attemptCount = 0;
+            while (x == null || !char.IsDigit(x.Value.KeyChar))
+            {
+                if (attemptCount > 4)
+                {
+                    Console.WriteLine("You Suck!");
+                    break;
+                }
+                Console.WriteLine("Enter a number:");
+                do
+                {
+                    x = Console.ReadKey();
+                } while (char.IsControl(x.Value.KeyChar));
+
+                Console.Write(Environment.NewLine);
+                attemptCount++;
+
+            }
+
         }
     }
+
 }
